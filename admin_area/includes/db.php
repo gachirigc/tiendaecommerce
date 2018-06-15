86a2536ab4e97fbe1e4371c7de8cd1d02e5bc8ce
gachirigc <39502857+gachirigc@users.noreply.github.com>
2018-06-15 13:00:34 -0500

    Update db.php

--- admin_area/includes/db.php
+++ admin_area/includes/db.php
@@ -1,5 +1,5 @@
 <?php
-$con = mysqli_connect("localhost","root","root","contactw_ecommerce");
+$con = mysqli_connect("amazonebay-mysqldbserver.mysql.database.azure.com","mysqldbuser@amazonebay-mysqldbserver","Universidad2018","contactw_ecommerce");
 if(mysqli_connect_errno()){
     echo "The Connection was not established: " . mysqli_connect_error();
 }
