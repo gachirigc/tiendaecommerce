31077b7d47608672e82ec7ea9e0fb31a7f0b799b
gachirigc <39502857+gachirigc@users.noreply.github.com>
2018-06-27 14:10:00 -0500

    Update header.php

--- header.php
+++ header.php
@@ -128,8 +128,8 @@
 											<h5>SUBTOTAL: $2940.00</h5>
 										</div>
 										<div class="cart-btns">
-											<a href="#">View Cart</a>
-											<a href="#">Checkout  <i class="fa fa-arrow-circle-right"></i></a>
+											<a href="cart.php">Ver Carrito</a>
+											<a href="checkout.php">Pagar <i class="fa fa-arrow-circle-right"></i></a>
 										</div>
 									</div>
 								</div>
