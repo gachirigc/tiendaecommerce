75387982c94c40beb6f23b50b5a4d18e1f03edef
gachirigc <39502857+gachirigc@users.noreply.github.com>
2018-06-29 09:36:28 -0500

    Update customer_login.php

--- customer_login.php
+++ customer_login.php
@@ -1,31 +1,31 @@
 <?php 
 include("includes/db.php");
 ?>
-<div> 
+<div id="content_area"> 
 	<form method="post" action=""> 
 		<table width="500" align="center" bgcolor="skyblue"> 
 			<tr align="center">
-				<td colspan="3"><h2>Login or Register to Buy!</h2></td>
+				<td colspan="3"><h2>Iniciar sesión o registrarse para comprar!</h2></td>
 			</tr>
 			<tr>
 				<td align="right"><b>Email:</b></td>
-				<td><input type="text" name="email" placeholder="enter email" required/></td>
+				<td><input type="text" name="email" placeholder="ingrese email" required/></td>
 			</tr>
 			
 			<tr>
 				<td align="right"><b>Password:</b></td>
-				<td><input type="password" name="pass" placeholder="enter password" required/></td>
+				<td><input type="password" name="pass" placeholder="ingrese password" required/></td>
 			</tr>
 			
 			<tr align="center">
-				<td colspan="3"><a href="checkout.php?forgot_pass">Forgot Password?</a></td>
+				<td colspan="3"><a href="checkout.php?forgot_pass">Se te olvidó tu contraseña?</a></td>
 			</tr>
 			
 			<tr align="center">
 				<td colspan="3"><input type="submit" name="login" value="Login" /></td>
             </tr>
         </table> 
-			<h2 style="float:right; padding-right:20px;"><a href="customer_register.php" style="text-decoration:none;">New? Register Here</a></h2>
+			<h2 style="float:right; padding-right:20px;"><a href="customer_register.php" style="text-decoration:none;">¿Nuevo? Registrar aquí</a></h2>
 	</form>
 	
 	<?php
