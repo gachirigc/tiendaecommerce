62e002f2a556e4c84a0efc576db76fdd1975fc2e
gachirigc <39502857+gachirigc@users.noreply.github.com>
2018-06-29 09:22:49 -0500

    Update checkout.php

--- checkout.php
+++ checkout.php
@@ -10,62 +10,20 @@ include("functions/functions.php");
     </head>
     
     <body>
-        <!-- Main Container starts here-->
-        <div class="main_wrapper">
-            <!-- Header starts here-->
-            <div class="header_wrapper">
-                <a href="index.php"><img id="logo" src="images/logo.gif"/></a>
-                <img id="banner" src="images/ad-banner.gif"/>
-            </div>
-            <!-- Header ends here-->
-            
-            <!-- Navegation Bar starts here-->
-            <div class="menubar">
-                <ul id="menu"><!-- 无序列表-->
-                    <li><a href="index.php">Home</a></li>
-				    <li><a href="all_products.php">All Products</a></li>
-				    <li><a href="customers/my_account.php">My Account</a></li>
-				    <li><a href="customer_register.php">Sign Up</a></li>
-				    <li><a href="cart.php">Shopping Cart</a></li>
-				    <li><a href="contact_us.php">Contact Us</a></li>
-                    <div id="form">
-                        <form method="get" action="results.php" enctype="multipart/form-data">
-                            <input type="text" name="user_query" placeholder= "Search a Product"/>
-                            <input type="submit" name="search" value="Search"/>
-                        </form>
-                    </div>
-                </ul>
-
-            </div>
-            <!-- Navegation Bar ends here-->
-            
-            <!-- Content starts here-->
+        <?php include("header.php");?>
             <div class="content_wrapper">
                 <div id="sidebar">
-                    <div id="sidebar_title">Categories</div>
+                    <div id="sidebar_title"><h2><br>Categorías</h2></div>
                     <ul id="cats">
                         <?php getCats(); ?>
                     </ul>
-                    <div id="sidebar_title">Brands</div>
+                    <div id="sidebar_title" style="padding:20px 0 5px 10px"><h2>Marcas</h2></div>
                     <ul id="cats">
                         <?php getBrands(); ?>
                     </ul>
                 </div>
                 <?php cart(); ?>
-                <div id="shopping_cart">
-                    <span style="float:right; font-size:18px; padding:5px; line-height:40px;">
-                        <?php 
-                            if(isset($_SESSION['customer_email'])){
-                                echo "<b>Welcome:</b>" . $_SESSION['customer_email'] . "<b style='color:yellow;'>Your</b>" ;
-                            }else {
-                                echo "<b>Welcome Guest:</b>";
-                            }
-                        ?>
-                        <b style="color:yellow">Shopping Cart-</b>Total Items: <?php total_items(); ?>  Total Price: <?php total_price(); ?><a href="cart.php" style="color:yellow"> Go to Cart</a>
-                    </span>
-                </div>
-                <div id="content_area">
-                    
+                <div id="content_area">                    
                     <div id="products_box">
                         <?php 
                         if(!isset($_SESSION['customer_email'])){
@@ -77,16 +35,9 @@ include("functions/functions.php");
                     </div>
                 </div>
             </div>
-            <!-- Content ends here-->
-            
+            <!-- Content ends here-->            
             <!-- Footer starts here-->
-            <div id="footer">
-                <h2 style="text-align: center; padding-top: 30px;">&copy;
-                    2016 by edward-shi<!-- &copy元素用于通知版权资料-->
-                </h2>
-            </div>
             <!-- Footer ends here-->
-        </div>
         <!-- Main Container ends here-->
     </body>
 
